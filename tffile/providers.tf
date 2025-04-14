b9fa73fda1350ffd31cbe0898c9eea506d2606a5
VirexSyscom <539401@syscom.com.tw>
2025-04-14 14:45:21 +0800

    87.5

--- tffile/providers.tf
+++ tffile/providers.tf
@@ -3,7 +3,7 @@ terraform {
     use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
     tenant_id            = "virex.online"                          # Can also be set via `ARM_TENANT_ID` environment variable.
     client_id            = "a9653185-f78e-477c-acfe-cfb167f2ef20"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    client_secret        =  $(ARM_CLIENT_ID)                       # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    client_secret        = "$(ARM_CLIENT_ID)"                      # Can also be set via `ARM_CLIENT_SECRET` environment variable.
     resource_group_name  = "$(RG)"
     storage_account_name = "$(SAName)"                             # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "$(ContainerName)"                      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.

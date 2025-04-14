71f3908f787692aa08244f416a0111a48256d5f0
VirexSyscom <539401@syscom.com.tw>
2025-04-14 11:46:42 +0800

    modify providers 2.0

--- tffile/providers.tf
+++ tffile/providers.tf
@@ -1,14 +1,12 @@
 terraform {
   backend "azurerm" {
-    tenant_id            = "$(TENANT_ID)"
-    client_id            = "$(ARM_CLIENT_ID)"
-    client_secret        = "$(ARM_CLIENT_SECRET_ID)"
-    subscription_id      = "$(SUB_ID)"
-    storage_account_name = "$(SAName)"
-    container_name       = "$(ContainerName)"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-    use_azuread_auth     = true
+    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    tenant_id            = "$(TENANT_ID)"                          # Can also be set via `ARM_TENANT_ID` environment variable.
+    client_id            = "$(ARM_CLIENT_ID)"                      # Can also be set via `ARM_CLIENT_ID` environment variable.
+    client_secret        = "$(ARM_CLIENT_SECRET_ID)"               # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    storage_account_name = "$(SAName)"                             # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "$(ContainerName)"                      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
   required_providers {
     azapi = {

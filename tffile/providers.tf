58c966af59a39a0d7ab13e1274364aa5eaf1e285
VirexSyscom <539401@syscom.com.tw>
2025-04-14 13:43:14 +0800

    modify 5.0

--- tffile/providers.tf
+++ tffile/providers.tf
@@ -4,11 +4,12 @@ terraform {
     tenant_id            = "$(TENANT_ID)"                          # Can also be set via `ARM_TENANT_ID` environment variable.
     client_id            = "$(ARM_CLIENT_ID)"                      # Can also be set via `ARM_CLIENT_ID` environment variable.
     client_secret        = "$(ARM_CLIENT_SECRET_ID)"               # Can also be set via `ARM_CLIENT_SECRET` environment variable.
+    resource_group_name  = "$(RG)"
     storage_account_name = "$(SAName)"                             # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "$(ContainerName)"                      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
-  
+
   required_providers {
     azapi = {
       source  = "azure/azapi"

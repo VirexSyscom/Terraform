3699ffaf82e5dd26b655dc98241d85778eb6460a
virex <539401@syscom.com.tw>
2025-05-09 08:54:50 +0800

    Create tffiledownload.sh

---
+++ tffiledownload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $(System.DefaultWorkingDirectory)
+wget https://raw.githubusercontent.com/VirexSyscom/Terraform/refs/heads/main/main.tf
+wget https://raw.githubusercontent.com/VirexSyscom/Terraform/refs/heads/main/output.tf
+wget https://raw.githubusercontent.com/VirexSyscom/Terraform/refs/heads/main/providers.tf
+wget https://raw.githubusercontent.com/VirexSyscom/Terraform/refs/heads/main/ssh.tf
+wget https://raw.githubusercontent.com/VirexSyscom/Terraform/refs/heads/main/variables.tf
